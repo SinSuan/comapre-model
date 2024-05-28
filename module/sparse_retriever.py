f530d19ac19b4018927a86528cd00e6689fc19f6
SinSuan <vicky0002@gmail.com>
2024-05-28 09:18:08 +0000

    init

---
+++ module/sparse_retriever.py
@@ -0,0 +1,52 @@
+import json
+from pyserini.search.lucene import LuceneSearcher
+from pyserini.index import IndexReader
+import sys
+import configparser
+
+
+
+sys.path.append("..")
+
+
+# config 讀取資訊
+config = configparser.ConfigParser()
+config.read('config.ini')
+inverted_index_filepath = config['filepath']['inverted_index']
+
+print('Loading Inverted Index for 神農gpt...')
+
+searcher = LuceneSearcher(inverted_index_filepath)
+index_reader = IndexReader(inverted_index_filepath)
+
+
+
+def sparse_retriever(question, k=40) -> list[str]:
+    searcher.set_language('zh')
+    hits = searcher.search(question, k+10)
+    candicate_context = []
+    candicate_info = []
+
+    for i in hits:
+        _content = json.loads(i.raw)["contents"]
+        _author = json.loads(i.raw)["author"]
+        _url = json.loads(i.raw)["url"]
+        _type = json.loads(i.raw)["type"]
+        _date = json.loads(i.raw)["date"]
+        _source = json.loads(i.raw)["source"]
+        
+        info_dic = {
+            'author':_author,
+            'contents':_content,
+            'date':_date,
+            'url':_url,
+            'type':_type,
+            'source':_source
+        }
+
+
+        if _content not in candicate_context:
+            candicate_context.append(_content)
+            candicate_info.append(info_dic)
+
+    return candicate_context,  candicate_info[:k]
