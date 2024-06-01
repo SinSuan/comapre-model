b002ce391bf096de9cad8f93b7b5429f6071c3b4
SinSuan <vicky0002@gmail.com>
2024-06-01 05:26:26 +0000

    完成 task_handler

--- module_test/about_model/call_model.py
+++ module_test/about_model/call_model.py
@@ -1,8 +1,11 @@
-import requests
-import re
-import json
+"""
+create parameters for various models and call the models
+"""
+
 import configparser
+import json
 import os
+import requests
 
 token = os.getenv('taide_api_key')
 
@@ -12,105 +15,23 @@ config.read('./config.ini')
 host_tsg = config['host']['nstc_taide']
 host_txl = config['host']['nlplab_taide_llama3']
 host_braw = config['host']['nlplab_breeze']
-
-# def call_tsg(full_prompt):
-#     """call api taide e.1.1.0-SG
-#     """
-#     headers = {
-#         "Authorization": "Bearer "+token
-#     }
-#     host = host_tsg
-#     data = {
-#         "model": "TAIDE/e.1.1.0-SG",
-#         "prompt": full_prompt,
-#         "temperature": 0.9,
-#         "top_p": 0.9,
-#         "presence_penalty": 1,
-#         "frequency_penalty": 1,
-#         "max_tokens": 200,
-#         "repetition_penalty":1.2
-#     }
-#     try:
-#         r = requests.post(host+"/completions", headers=headers, json=data)
-#         result = r.json()["choices"][0]["text"]
-#         return result
-
-#     except Exception as e:
-#         print(f"發生錯誤:{e}")
-#         print(r.json())
-#         return ""
-
-# def call_txl(full_prompt):
-#     """ call api taide Llama3-TAIDE-LX-8B-Chat-Alpha1
-#     """
-#     headers = {
-#         'Content-Type': 'application/json',
-#         'accept': 'application/json'
-#     }
-#     parameters = {
-#         "temperature": 0.9,
-#         "top_p": 0.9,
-#         "max_new_tokens": 200,
-#     }
-#     payload = json.dumps({
-#         "inputs": full_prompt,
-#         "parameters": parameters,
-#     })
-
-#     try:
-
-#         r = requests.request("POST", host_txl, headers=headers, data=payload)
-#         result = json.loads(r.text)["generated_text"]
-
-#         return result
-
-#     except Exception as e:
-#         print(f"發生錯誤:{e}")
-#         print(r.json())
-#         return ""
-
-# def call_braw(full_prompt):
-#     """ call api Breeze-7B-Instruct-v1_0
-#     """
-#     headers = {
-#         'Content-Type': 'application/json',
-#         'accept': 'application/json'
-#     }
-#     parameters = {
-#         "do_sample": True,
-#         "top_p": 0.95,
-#         "temperature": 0.01
-#     }
-#     payload = json.dumps({
-#         "inputs": full_prompt,
-#         "parameters": parameters
-#     })
-#     try:
-#         r = requests.request("POST", host_braw, headers=headers, data=payload)
-#         result = json.loads(r.text)["generated_text"]
-#         return result
-
-#     except Exception as e:
-#         print(f"發生錯誤:{e}")
-#         print(r.text)
-#         return 'exception occurred'
+host_bcot = config['host']['nlplab_breeze_cot']
 
 def call_model(model_type, full_prompt):
     """ general model caller for various models
     """
     try:
-        
-        host, headers, data = param_4_tsg(full_prompt)
+
+        host, headers, data = get_param(model_type, full_prompt)
         if model_type == "tsg":
             r = requests.post(host+"/completions", headers=headers, json=data)
             result = r.json()["choices"][0]["text"]
-        elif model_type in ["tsg", "braw"]:
+
+        elif model_type in ["tsg", "braw", "bcot"]:
             r = requests.request("POST", host_braw, headers=headers, data=data)
             result = json.loads(r.text)["generated_text"]
-        elif model_type == "bcot":
-            pass
 
-        return result
+        return str(result)
 
     except Exception as e:
         print(f"發生錯誤:{e}")
@@ -128,7 +49,7 @@ def get_param(model_type, full_prompt):
     elif model_type == "braw":
         host, headers, data = param_4_braw(full_prompt)
     elif model_type == "bcot":
-        pass
+        host, headers, data = param_4_bcot(full_prompt)
 
     return host, headers, data
 
@@ -192,4 +113,19 @@ def param_4_braw(full_prompt):
 def param_4_bcot(full_prompt):
     """ define parameters for Breeze-v1_0-CoT-v0_2-full
     """
-    pass
+    host = host_braw
+    headers = {
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }
+    parameters = {
+        "do_sample": True,
+        "top_p": 0.95,
+        "temperature": 0.01,
+        "max_new_tokens":2000 # the number of output text
+    }
+    data = json.dumps({
+        "inputs": full_prompt,
+        "parameters": parameters
+    })
+    return host, headers, data
---
+++ module_test/task_handler.py
@@ -0,0 +1,86 @@
+""" handle the following task
+
+        cls   question_classification
+        hyde  question_expansion
+        sum   summary_generator
+"""
+
+import re
+from about_model.call_model import call_model
+from about_model.create_full_prompt import prompt_creator
+
+def handle_task(task_type, model_type, query, answers=None):
+    """ get task system prompt
+    Var:
+        task_type: str
+            cls   question_classification
+            hyde  question_expansion
+            sum   summary_generator
+    Return:
+        String
+    """
+    system_prompt = get_system_prompt(task_type)
+    user_prompt = get_user_prompt(task_type, query, answers)
+    full_prompt = prompt_creator(model_type, system_prompt, user_prompt)
+    response = call_model(model_type, full_prompt)
+
+    if task_type in ["hyde", "sum"]:
+        result = response
+
+    elif task_type=="cls":
+        numbers = re.findall(r'\d+', response)
+        # re.findall 返回的是list，直接取出
+        type_num = 0
+        if len(numbers) > 0:
+            num = int(numbers[0])
+            if 0 <= num <=2:
+                type_num = num
+        question_type_category = ["Fact Verification", "General QA", "Non-Agriculural Question"]
+
+        result = question_type_category[type_num]
+
+    return result
+
+def get_system_prompt(task_type:str) -> str:
+    """ get task system prompt
+    Var:
+        task_type: str
+            cls   question_classification
+            hyde   question_expansion
+            sum   summary_generator
+    
+    Return:
+        String
+    """
+
+    if task_type=="cls":
+        system_prompt ="""你現在是一位農業專家，請對輸入問題進行三種分類。農業事實驗證問題(例子:水稻稻熱病是由水稻稻熱病病毒所引起的嗎？、農藥會造成植物根部萎縮？)、\
+                        農業知識型問答(例子:水稻稻熱病的可能原因?、如何提高作物的產量？)、\
+                        非農業問題(例子:什麼是歷史確定論，中興大學位於哪裡，國泰信用卡申辦規則有哪些。)，\
+                        你的回答只能是一個數字，有三種: 0、1、2 ，如果是農業事實驗證問題請回答0 ，如果是農業一般問答請回答1 ，如果非農業問題請回答2。\
+        """
+    elif task_type=="hyde":
+        system_prompt = "你是一個農業專家，請幫我解答以下問題，請用中文回答。"
+    elif task_type=="sum":
+        system_prompt = "你是一個農業專家，請幫我完成任務以下問題，請用中文回答。"
+
+    return system_prompt
+
+def get_user_prompt(task_type, query, answers=None):
+    """ get task system prompt
+    Var:
+        task_type: str
+            cls   question_classification
+            hyde   question_expansion
+            sum   summary_generator
+    
+    Return:
+        String
+    """
+
+    if task_type in ["cls", "hyde"]:
+        user_prompt = query
+    elif task_type=="sum":
+        user_prompt = f"我有以下片段關於{query}的答案：{str(answers)} 你可以試著幫我將結果進行統整摘要嗎？"
+
+    return user_prompt
